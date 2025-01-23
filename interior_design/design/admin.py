e32b162644cb1d92d42502b34d89e4e04e872182
Sonali504 <aswalmansi1680@gmail.com>
2025-01-23 12:08:41 +0530

    new

--- interior_design/design/admin.py
+++ interior_design/design/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Project, Testimonial, Contact
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Testimonial)
+admin.site.register(Contact)
--- interior_design/design/models.py
+++ interior_design/design/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    category = models.CharField(max_length=100, choices=[('Residential', 'Residential'), ('Commercial', 'Commercial')])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Testimonial(models.Model):
+    client_name = models.CharField(max_length=100)
+    feedback = models.TextField()
+    image = models.ImageField(upload_to='clients/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.client_name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
